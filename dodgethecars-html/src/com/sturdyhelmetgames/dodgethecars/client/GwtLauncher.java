63bfa028ec7acb2e6860c7f395926f57af89756d
Antti Kolehmainen <antionio@gmail.com>
2012-10-07 21:49:58 +0300

    Updated libs AGAIN :) Also fixed GWT includes.

--- dodgethecars-html/src/com/sturdyhelmetgames/dodgethecars/client/GwtLauncher.java
+++ dodgethecars-html/src/com/sturdyhelmetgames/dodgethecars/client/GwtLauncher.java
@@ -1,14 +1,15 @@
 package com.sturdyhelmetgames.dodgethecars.client;
 
-import com.sturdyhelmetgames.dodgethecars.DodgeTheCarsGame;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.sturdyhelmetgames.dodgethecars.DodgeTheCarsGame;
 
 public class GwtLauncher extends GwtApplication {
 	@Override
 	public GwtApplicationConfiguration getConfig() {
-		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(800, 400);
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(800,
+				480);
 		return cfg;
 	}
 
